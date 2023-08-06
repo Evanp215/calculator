7f06cccfe8b553f404521eec092d46eb4d8368b6
Evanp215 <epybas215@gmail.com>
2023-08-06 18:25:45 -0400

    created functions for all opperators, clear, and equals signs that call on button click

--- script.js
+++ script.js
@@ -1,16 +1,60 @@
-document.addEventListener("DOMContentLoaded", function() {
-  let screenDisplay = document.querySelector('#screen-display');
-  screenDisplay.textContent = '';
+let currentValue = '';
+let nextValue = '';
+// Define screenDisplay and set text-content to empty string
+let screenDisplay = document.querySelector('#screen-display');
+screenDisplay.textContent = '';
 
-  let buttons = document.querySelectorAll('.calc-button');
-  buttons.forEach((button) => {
-    button.addEventListener('click', function() {
-      if(button.id == 'clear') {
-        screenDisplay.textContent = ''
-      }
-      else {
-        screenDisplay.textContent += `${button.textContent}`
-      }
-    });
+// add event listener to each button
+let buttons = document.querySelectorAll('.calc-button');
+buttons.forEach((button) => {
+  button.addEventListener('click', function() {
+    if(button.id === 'clear') {
+      clear();
+    }
+    else if(button.id === 'add') {
+      add();
+    }
+    else if(button.id === 'subtract') {
+      subtract();
+     }
+     else if(button.id === 'multiply') {
+      multiply();
+     }
+    else if(button.id === 'divide') {
+      divide();
+     }
+     else if(button.id === 'equal') {
+      equals();
+     }
+     else {
+       screenDisplay.textContent += button.textContent;
+     }
   });
 });
+
+
+function clear() {
+  screenDisplay.textContent = '';
+  currentValue = '';
+  nextValue = '';
+}
+
+function add(current, next) {
+  return current + next;
+}
+
+function subtract(current, next) {
+  return current - next;
+}
+
+function multiply(current, next) {
+  return current * next;
+}
+
+function divide(current, next) {
+  return current / next;
+}
+
+function equals() {
+  
+}
